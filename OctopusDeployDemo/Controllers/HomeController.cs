3ccbad7787d0225c0cac9a2d32da7b3e1974ecae
Rob Moore <me@robdmoore.id.au>
2015-10-06 11:57:52 +0800

    Adding demo site

---
+++ OctopusDeployDemo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Web.Mvc;
+
+namespace OctopusDeployDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.PropertyFromConfig = ConfigurationManager.AppSettings["SomeProperty"];
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OctopusDeployDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OctopusDeployDemo.MvcApplication" Language="C#" %>
